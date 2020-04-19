f9252883145e62c6aebce7146699bc077f3496b4
Arnold Daniels <arnold@jasny.net>
2020-04-19 08:11:27 +0200

    Replace dynamicReturnTypeMeta.json with .phpstorm.meta.php

---
+++ .phpstorm.meta.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * PhpStorm code completion
+ */
+
+namespace PHPSTORM_META {
+    override(\Jasny\Autowire\AutowireInterface::get(0),
+        map([
+            '' => '@',
+        ])
+    );
+}
+
